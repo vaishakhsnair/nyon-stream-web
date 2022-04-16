caccc5cf81b2b2c37bdcfb5ad9ad24c10d43e243
vaishakhsnair <vaishakhsnair2004@gmail.com>
2022-04-16 23:59:29 +0530

    fixing stuff

--- static/player.js
+++ static/player.js
@@ -1,30 +1,35 @@
 
-      window.addEventListener('load',function(){
-        socks("open");
-       } )
+const uid = document.cookie.split("=")[1];
+const keepaliveinterval = 5;
+const params = new URLSearchParams(window.location.search);
+const magnet = params.get("magnet");
+
+window.addEventListener('load',function(){
+   socks("started");
+   } )
 
 
-      function socks(message) {
-         var ws = new WebSocket("ws://localhost:8080/ws");
-         var blob = new Blob([JSON.stringify({"message":message,"uid":uid,"thread":threadid})],{type:'application/json; charset=UTF-8'});
+function socks(message) {
+   var ws = new WebSocket("ws://localhost:8080/ws");
+   var blob = new Blob([JSON.stringify({"message":message,"uid":uid,"magnet":magnet})],{type:'application/json; charset=UTF-8'});
 
-         ws.onopen = function() {
-            ws.send(blob);
-         }
-         ws.onmessage = function(event) {
-            var cont = event.data;
-            console.log(cont);          
-           // ws.close();
-         
-         }
-         ws.onclose = function() {
-            console.log("Closed");            
-         }
-         
-      }
+   ws.onopen = function() {
+      ws.send(blob);
+   }
+   ws.onmessage = function(event) {
+      var cont = event.data;
+      console.log(cont);          
+      // ws.close();
+   
+   }
+   ws.onclose = function() {
+      console.log("Closed");            
+   }
+   
+}
 
-      keepalivetimer = setInterval(function() {
+keepalivetimer = setInterval(function() {
 
-         socks("keepalive")
+   socks("keepalive")
 
-      },keepaliveinterval*1000)
+},keepaliveinterval*1000)
