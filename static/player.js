e6af6ef3e8d867764e64a5dcc7195ca9608de9ce
vaishakhsnair <vaishakhsnair2004@gmail.com>
2022-04-21 13:12:02 +0530

    yes

--- static/player.js
+++ static/player.js
@@ -1,32 +1,63 @@
 
 const uid = document.cookie.split("=")[1];
-const keepaliveinterval = 5;
+const keepaliveinterval = 10;
 const params = new URLSearchParams(window.location.search);
 const magnet = params.get("magnet");
-
-window.addEventListener('load',function(){
-   socks("started");
-   } )
-
+var srcset = false;
 
 function socks(message) {
-   var ws = new WebSocket("ws://localhost:8080/ws");
-   var blob = new Blob([JSON.stringify({"message":message,"uid":uid,"magnet":magnet})],{type:'application/json; charset=UTF-8'});
+   var blob = JSON.stringify({"message":message,"uid":uid,"magnet":magnet})
 
-   ws.onopen = function() {
+   if (ws) {
       ws.send(blob);
-   }
-   ws.onmessage = function(event) {
-      var cont = event.data;
-      console.log(cont);          
-      // ws.close();
-   
    }
    ws.onclose = function() {
       console.log("Closed");            
    }
    
 }
+
+window.addEventListener('load',function(){
+   ws = new WebSocket("ws://"+window.location.hostname+":8080/ws");
+   if (ws) {
+      ws.onopen = function() {
+         console.log("Connected to Server");
+         socks("started");
+         socks("keepalive");
+      }
+
+      ws.onclose = function(e) {
+         console.log("Connection closed (wasClean = " + e.wasClean + ", code = " + e.code + ", reason = '" + e.reason + "')");
+         
+      }
+
+      ws.onmessage = function(e) {
+         var data = JSON.parse(e.data);
+         console.log("Got echo: " + e.data);
+         if (data.message==="ready"){
+           var addr = "http://"+window.location.hostname+":"+data.port+"/"+data.addr[0];
+           setplayer(addr);
+           }
+        }
+
+   }
+})
+
+
+
+
+function setplayer(addr) {
+   var player = document.getElementById("player");
+   var playersrc = document.getElementById("playersrc");
+   playersrc.setAttribute("src",addr);
+   if (srcset != true){
+      player.load();
+      srcset = true;
+   }
+
+}
+
+
 
 keepalivetimer = setInterval(function() {
 
