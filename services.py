140d08576729f99455e30518b01f86106ae183d6
vaishakhsnair <vaishakhsnair2004@gmail.com>
2022-05-23 21:00:18 +0530

    seperated service to new module  and added plugins to plugin folder

---
+++ services.py
@@ -0,0 +1,108 @@
+
+
+import json
+import subprocess
+import datetime
+import time
+import random
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+#External Plugins
+import plugins.nyaa.nyaascraper as nyaa
+import plugins.animetosho.animetoshoapi as tosho
+
+
+
+active = {}             #keeping track of active user's services
+keepalivehistory = {}   #keeping track of active online users
+
+
+def scrape(payload):
+    global active,keepalivehistory
+    uid = payload["uid"]
+    if "addr" not in active[uid].keys():
+        try:
+            port = active[uid]["port"]
+        except KeyError:
+            return ["Waiting","Null","Null"]
+        time.sleep(3)
+        resp = None
+        while resp==None:
+            try:
+                resp = requests.get(f"http://localhost:{port}")
+            except:
+                pass
+
+            if uid not in active.keys():     
+                print("UID Not Found Cancelling scrape")     #avoid zombie mode if the main process is terminated before this thread
+                return None
+
+        soup =  bs(resp.text, 'html.parser')
+        li = soup.find_all("a",href=True)
+        li = [i["href"] for i in li]
+        print(li)
+        active[uid]["addr"] = li
+        active[uid]["scraped"] = True
+        return ["ready",li,port]
+    else:   
+        return ["ready",active[uid]["addr"],active[uid]["port"]]
+
+def ready_subs(payload):
+    global active
+    extract_path = tosho.get_subs(int(payload["nyaaid"]))
+    if extract_path == -1:
+        active[payload["uid"]]["subtitles"] = "Not Available"
+    else:
+        active[payload["uid"]]["subtitles"]=extract_path
+    print(active[payload["uid"]]["subtitles"])
+
+def keepalivecheck():
+    global keepalivehistory
+    time.sleep(15)
+    if keepalivehistory:
+        print(keepalivehistory)
+        torem = []
+        for i in keepalivehistory.keys():
+            last_ping = keepalivehistory[i]
+            if ((datetime.datetime.now() - last_ping).seconds) > 15:
+                print(f"UID : {i} is stale , killing process")
+                print(active)
+                if i in active.keys():
+                    active[i]["process"].kill()
+                    del(active[i])
+                    torem.append(i)
+                else:
+                    print("UID Not in active sessions")
+        if torem:
+            for i in torem:
+                del(keepalivehistory[i])
+                
+def getport():
+    while True:
+        port = random.randint(8081,9999)
+        activeports = [i["port"] if i != None else None for i in active.values()]
+        print(activeports)
+        if port not in activeports:
+            return port
+    
+
+def player(uid,magnet,configs,webplayer_args):
+    global active
+    print(active)
+    port = getport()
+    print("Port :",port)    
+    if active[uid] is None:     
+        print("Not Running")   
+    else:
+        active[uid]["process"].kill()
+
+    #print("Command :",f"{configs['node_path']} {configs['webtorrent_path']} \"{magnet}\" {webplayer_args} -p {port}")
+    process = subprocess.Popen(f"{configs['node_path']} {configs['webtorrent_path']} \"{magnet}\" {webplayer_args} -p {port}",shell=True
+                                ,stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT,bufsize=0)
+
+    active[uid] = {"port":port,"magnet":magnet,"process":process,"scraped":"Not Started","subtitles":"Not Started"}
+    print(active[uid])
+
+    #stdout=subprocess.PIPE
--- web.py
+++ web.py
@@ -2,6 +2,7 @@
 import datetime
 import json
 import random
+import random
 import subprocess
 import sys
 import threading
@@ -23,21 +24,12 @@ from twisted.python import log
 from twisted.web.server import Site
 from twisted.web.wsgi import WSGIResource
 
-import nyaa.nyaascraper as nyaa
-import animetosho.animetoshoapi as tosho
+#Background service modules
+import services
 
 print("Nyon Stream Web Server")
-nyaa.node_path = os.path.join("dependencies","node.exe")
-nyaa.webtorrent_path = os.path.join("dependencies","webtorrent-cli","bin","cmd.js")
 
-webplayer_args = "--playlist --keep-seeding -o torrents/"
 
-if os.name=="posix":
-    nyaa.node_path = ""
-    nyaa.webtorrent_path = r"webtorrent"
-
-active = {}
-keepalivehistory = {}
 # Our WebSocket Server protocol
 class socks(WebSocketServerProtocol):
 
@@ -47,41 +39,41 @@ class socks(WebSocketServerProtocol):
     def onMessage(self, payload, isBinary):
         self.sendMessage(payload, isBinary)
         payload = json.loads(payload.decode("utf-8"))        
-        print(payload)
+        #print(payload)
 
         if payload["message"] == "started":
-            player(uid=payload["uid"],magnet = payload["magnet"])
+            services.player(uid=payload["uid"],magnet = payload["magnet"],configs=configs,webplayer_args=webplayer_args)
         
         if payload["message"] == "keepalive":
-            print(active[payload["uid"]].keys(),[i.name for i in threading.enumerate()],f'scrape-{payload["uid"]}',sep="\n")
+            print(services.active[payload["uid"]].keys(),[i.name for i in threading.enumerate()],f'scrape-{payload["uid"]}',sep="\n")
 
-            if  active[payload["uid"]]["scraped"] == "Not Started":
+            if  services.active[payload["uid"]]["scraped"] == "Not Started":
                 print("Not scraped or scraping not completed")
-                threading.Thread(name=f'scrape-{payload["uid"]}', target=scrape,args=(payload,)).start()
+                threading.Thread(name=f'scrape-{payload["uid"]}', target=services.scrape,args=(payload,)).start()
                 self.sendMessage(json.dumps({"message":"Not Ready","uid":payload["uid"]}).encode(),isBinary)
 
                 if "nyaaid" in payload.keys():
-                    threading.Thread(name=f'subs-{payload["uid"]}',target=ready_subs,args=(payload,)).start()
-                    active[payload["uid"]]["subtitles"] = "running"
+                    threading.Thread(name=f'subs-{payload["uid"]}',target=services.ready_subs,args=(payload,)).start()
+                    services.active[payload["uid"]]["subtitles"] = "running"
 
-                active[payload["uid"]]["scraped"] = "running"
+                services.active[payload["uid"]]["scraped"] = "running"
 
-            elif active[payload["uid"]]["scraped"] == "running" or active[payload["uid"]]["subtitles"] == "running":
+            elif services.active[payload["uid"]]["scraped"] == "running" or services.active[payload["uid"]]["subtitles"] == "running":
                 print("scraping/Subs not completed")
 
             else:
                 print("Scrape completed")    
-                message = ["ready",active[payload["uid"]]["addr"],active[payload["uid"]]["port"],active[payload["uid"]]["subtitles"]]    
+                message = ["ready",services.active[payload["uid"]]["addr"],services.active[payload["uid"]]["port"],services.active[payload["uid"]]["subtitles"]]    
                 print(message)    
                 self.sendMessage(json.dumps({"message":message[0],"uid":payload["uid"],
                                 "addr":message[1],"port":message[2],"subtitles":message[3]}).encode(),isBinary)
                 print("Changing Keepalive")
-                keepalivehistory[payload["uid"]] = datetime.datetime.now()
+                services.keepalivehistory[payload["uid"]] = datetime.datetime.now()
 
     def onClose(self, wasClean, code, reason):
         print(code,reason)
         if "kill" not in [i.name for i in threading.enumerate()]:
-            threading.Thread(name="kill",target=keepalivecheck).start()
+            threading.Thread(name="kill",target=services.keepalivecheck).start()
 
 
 class statsocks(WebSocketServerProtocol):
@@ -89,95 +81,6 @@ class statsocks(WebSocketServerProtocol):
         print("Client connect",request.peer)
     
     
-
-
-
-def scrape(payload):
-    global active
-    uid = payload["uid"]
-    if "addr" not in active[uid].keys():
-        try:
-            port = active[uid]["port"]
-        except KeyError:
-            return ["Waiting","Null","Null"]
-        time.sleep(3)
-        resp = None
-        while resp==None:
-            try:
-                resp = requests.get(f"http://localhost:{port}")
-            except:
-                pass
-
-            if uid not in keepalivehistory.keys():     
-                print("UID Not Found Cancelling scrape")     #avoid zombie mode if the main process is terminated before this thread
-                #return None
-
-        soup =  bs(resp.text, 'html.parser')
-        li = soup.find_all("a",href=True)
-        li = [i["href"] for i in li]
-        print(li)
-        active[uid]["addr"] = li
-        active[uid]["scraped"] = True
-        return ["ready",li,port]
-    else:   
-        return ["ready",active[uid]["addr"],active[uid]["port"]]
-
-def ready_subs(payload):
-    global active
-    extract_path = tosho.get_subs(int(payload["nyaaid"]))
-    if extract_path == -1:
-        active[payload["uid"]]["subtitles"] = "Not Available"
-    else:
-        active[payload["uid"]]["subtitles"]=extract_path
-    print(active[payload["uid"]]["subtitles"])
-
-def keepalivecheck():
-    time.sleep(15)
-    if keepalivehistory:
-        print(keepalivehistory)
-        torem = []
-        for i in keepalivehistory.keys():
-            last_ping = keepalivehistory[i]
-            if ((datetime.datetime.now() - last_ping).seconds) > 15:
-                print(f"UID : {i} is stale , killing process")
-                print(active)
-                if i in active.keys():
-                    active[i]["process"].kill()
-                    del(active[i])
-                    torem.append(i)
-                else:
-                    print("UID Not in active sessions")
-        if torem:
-            for i in torem:
-                del(keepalivehistory[i])
-                
-def getport():
-    while True:
-        port = random.randint(8081,9999)
-        activeports = [i["port"] if i != None else None for i in active.values()]
-        print(activeports)
-        if port not in activeports:
-            return port
-    
-
-def player(uid,magnet):
-    global active
-    print(active)
-    port = getport()
-    print("Port :",port)    
-    if active[uid] is None:     
-        print("Not Running")   
-    else:
-        active[uid]["process"].kill()
-
-    #print("Command :",f"{nyaa.node_path} {nyaa.webtorrent_path} \"{magnet}\" {webplayer_args} -p {port}")
-    process = subprocess.Popen(f"{nyaa.node_path} {nyaa.webtorrent_path} \"{magnet}\" {webplayer_args} -p {port}",shell=True
-                                ,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,bufsize=0)
-
-    active[uid] = {"port":port,"magnet":magnet,"process":process,"scraped":"Not Started","subtitles":"Not Started"}
-    print(active[uid])
-        
-
 # Our WSGI application .. in this case Flask based
 app = Flask(__name__)
 app.secret_key = str(uuid.uuid4())
@@ -194,18 +97,17 @@ def page_home():
     if request.method == "POST":
 
         request_query = request.form["query"]
-        torrents = nyaa.getTorrents(request_query.replace(" ","+"))
+        torrents = services.nyaa.getTorrents(request_query.replace(" ","+"))
         if len(torrents)>0:
             return jsonify({"message":"success","magnets":torrents})
         return jsonify({"message":"error","magnets":"No Results"})
 
 @app.route("/player")
 def player_page():
-    global active
     uid = uuid.uuid4()
     resp = make_response(render_template("player.html"))
     resp.set_cookie("uid",str(uid))
-    active[str(uid)] = None
+    services.active[str(uid)] = None
     return resp
 
 @app.route("/sus")
@@ -223,14 +125,28 @@ def returnsubs(flpath):
 def webtorrstats():
     uid = request.cookies.get("uid")
     print(uid)
-    if uid in active.keys():
-        a = active[uid]
+    if uid in services.active.keys():
+        a = services.active[uid]
         return json.dumps({"port":a["port"],"magnet":a["magnet"],"scraped":a["scraped"],"subtitles":a["subtitles"]})
 
     else:
         return json.dumps({"Message":"Not Found"})
 
 if __name__ == "__main__":
+
+    with open("config.json","r") as configfile:
+        configs = json.load(configfile)
+
+    webplayer_args = configs['webtorrent-arguments']
+
+    if os.name == "posix":
+        configs = configs["linux"]
+    else:
+        configs = configs["windows"]
+    
+    print("Current Config :",configs)
+
+    
 
     log.startLogging(sys.stdout)
 
